5a49815aec1a223c48dbbe674bc8e43544b6925e
Diego Aldao <diego_ind@live.com.ar>
2023-06-16 19:41:02 -0300

    agregado el componente seleccion, y un estado para que el fetch sepa si hacer el fetch a imagenes o videos

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import GaleriaHome from "@components/Home/GaleriaHome";
 import Header from "@components/Home/Header";
-import React from "react";
+import Seleccion from "@components/Home/Seleccion";
+import React, { useState } from "react";
 
 const Inicio = () => {
+  const [tipo, setTipo] = useState<string>("imagen");
   return (
     <>
       <Header />
-      <GaleriaHome />
+      <section className="px-3 py-5">
+        <Seleccion tipo={tipo} setTipo={setTipo} />
+        <GaleriaHome tipo={tipo} />
+      </section>
     </>
   );
 };
