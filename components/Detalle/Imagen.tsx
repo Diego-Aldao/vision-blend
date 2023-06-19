e03139ad2894602b53ae341e30b4f04dc856cd66
Diego Aldao <diego_ind@live.com.ar>
2023-06-19 20:47:43 -0300

    añadido un componente para renderizar la imagen principal de la pagina detalle

---
+++ components/Detalle/Imagen.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  imagenes: {
+    landscape: string;
+    large: string;
+    large2x: string;
+    medium: string;
+    original: string;
+    portrait: string;
+    small: string;
+    tiny: string;
+  };
+  alt: string;
+};
+
+const Imagen: React.FC<Props> = ({ imagenes, alt }) => {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <div className="w-full my-5  mx-auto px-3 relative sm:max-w-fit ">
+        <Image
+          src={imagenes.large2x}
+          alt={alt}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-auto object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Imagen;
