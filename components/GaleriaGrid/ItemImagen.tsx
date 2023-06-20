633c45aff4a8154e08e24e81e395a0a4e718af75
Diego Aldao <diego_ind@live.com.ar>
2023-06-20 20:16:36 -0300

    corregido el import del type UrlImagen y clases de display y color de bg

--- components/GaleriaGrid/ItemImagen.tsx
+++ components/GaleriaGrid/ItemImagen.tsx
@@ -1,13 +1,13 @@
 import { Icon } from "@iconify/react";
 import Image from "next/image";
 import React from "react";
-import { Imagenes } from "@customTypes/imagen";
+import { UrlImagenes } from "@customTypes/customTypes";
 
 type Props = {
   infoImagen: {
     id: number;
     alt: string;
-    src: Imagenes;
+    src: UrlImagenes;
     photographer: string;
     photographer_id: number;
     photographer_url: string;
@@ -18,7 +18,7 @@ type Props = {
 const ItemImagen: React.FC<Props> = ({ infoImagen, handleClick }) => {
   return (
     <div
-      className="w-full relative mb-5 rounded-md overflow-hidden cursor-pointer"
+      className="w-full relative mb-5 rounded-md overflow-hidden cursor-pointer transition-all"
       onClick={() => handleClick(infoImagen.id)}
     >
       <Image
@@ -29,22 +29,22 @@ const ItemImagen: React.FC<Props> = ({ infoImagen, handleClick }) => {
         sizes="100vw"
         className="w-full h-auto object-cover"
       />
-      <div className="p-5 flex flex-col w-full h-full absolute opacity-0  hover:opacity-100 hover:bg-black/25 z-10 top-0 left-0 transition-all justify-between">
+      <div className="hidden p-5 sm:flex flex-col w-full h-full absolute opacity-0  hover:opacity-100 hover:bg-black/25 z-10 top-0 left-0 transition-all justify-between">
         <div className="interaccion flex items-start justify-end w-full gap-3">
-          <span className="p-3 bg-white rounded">
+          <span className="p-3 bg-slate-50 rounded">
             <Icon
               icon="ic:outline-bookmarks"
               className=" text-gray-600 w-5 h-5"
             />
           </span>
-          <span className="p-3 bg-white rounded">
+          <span className="p-3 bg-slate-50 rounded">
             <Icon
               icon="mdi:cards-heart-outline"
               className=" text-gray-600 w-5 h-5"
             />
           </span>
         </div>
-        <p className="nombre text-white capitalize font-medium">
+        <p className="nombre text-slate-50 capitalize font-medium">
           {infoImagen.photographer}
         </p>
       </div>
