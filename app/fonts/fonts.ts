300d7a7337c38f3ee895db84dd1d630e8c65c206
Diego Aldao <diego_ind@live.com.ar>
2023-06-20 20:12:56 -0300

    añadidas las fuentes con next fonts

---
+++ app/fonts/fonts.ts
@@ -0,0 +1,10 @@
+import { Outfit, Cinzel_Decorative } from "@next/font/google";
+
+export const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export const cinzelDecorative = Cinzel_Decorative({
+  weight: ["700"],
+  subsets: ["latin"],
+});
